4bb255fb3b78cb8d091924ec615733639a2d9f42
Griger Ratia <grigerratia@gmail.com>
2023-06-08 03:07:49 -0400

    funcines de componentes - logo/agradecimientos-1

--- src/components/components/Logo.jsx
+++ src/components/components/Logo.jsx
@@ -1,12 +1,39 @@
+import { useState } from 'react'
 import '../../styles/styles-components/Logo.css'
+import THANKS from '../../utils/thanks'
 
 function Logo(){
+
+    const [className, setClassName] = useState('msg-desactive')
+    const [message, setMessage] = useState(THANKS[0])
+    const [aNew, setANew] = useState('new-news')
+
+    const vewMessage = () => {
+        className === 'msg-desactive'
+            ? (
+                setClassName('msg-active'),
+                setANew('not-news')
+            ) : (
+                setClassName('msg-desactive'),
+                setNotice()
+            )
+    }
+
+    const setNotice = () => {
+        setTimeout(()=> {
+            setMessage(THANKS[Math.floor(Math.random() * (9 - 0))])
+            setANew('new-news')
+        }, 90000)
+    }
+
     return (
-        <a href="#" className='logo'>
+        <div className='logo' onClick={ vewMessage } >
             <img src="https://thumbs2.imgbox.com/16/75/BrVkQVSF_t.jpg" alt="Logo Griger"/>
-            <div className='newKnows'>1</div>
-            <p>Toda la gloria para Dios</p>
-        </a>
+            <div className={ aNew }>1</div>
+            <a href='#' className={ className } >
+                { message }
+            </a>
+        </div>
     )
 }
 
---
+++ src/utils/thanks.jsx
@@ -0,0 +1,14 @@
+const THANKS = [
+    'A Dios, ¡gracias!. ',
+    '¡Toda la gloria para Dios!',
+    '¡En Dios confío, y solo a Él sirvo!',
+    'Lo que sé y lo que tengo, nunca fue por mí. Es Dios.',
+    'Para mi amada esposa, Idalys. ¡Gracias por tanto!',
+    'Para mi amada hija, Cesleste. Mi motivación.',
+    'Para mis padres. Lo han dado todo por mí y mis hermanos.',
+    'Para mis hermanos; siempre idiotas. ¡Su alfíl siempre!',
+    'Para mis hermanos en Cristo. De rodillas detrás de mí',
+    'Dios es soberano en mi vida. Decide por mí. Actúa por mí',
+]
+
+export default THANKS

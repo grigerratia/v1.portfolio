6c5cf154b9b590280c456d27f671c9c1b5a94725
Griger Ratia <grigerratia@gmail.com>
2023-06-10 02:03:56 -0400

    Mejoras variadas

--- src/components/layouts/GeneralSection.jsx
+++ src/components/layouts/GeneralSection.jsx
@@ -1,4 +1,5 @@
 import '../../styles/styles-layout/GeneralSection.css'
+import { MdCloudDownload } from 'react-icons/md'
 import Details from '../components/Details'
 
 function GeneralSection() {
@@ -9,12 +10,17 @@ function GeneralSection() {
                         <img src="https://unavatar.io/twitter/grigerratia" alt="my-photo" />
                         <h1>Griger Ratia</h1>
                     </div>
-                    <h2>Desarrollador Web</h2>
+                    <div className="download_cv">
+                        <h2>Desarrollador Web</h2>
+                        <a href='http://127.0.0.1:5173/src/assets/Curriculum-Griger-06-23.pdf' target='_blank'>
+                            <MdCloudDownload />
+                        </a>
+                    </div>
                 </div>
                 <hr />
                 <Details>
                     <p className='details'>
-                        Soy desarrollador Web, especializado en los Stacks MERN y MEVN. Inicié mis estudios en <a href='https://platzi.com/p/Griger/'>Platzi</a>, donde desarrollé mis capacidades para crear interfaces de usuario usando buenas prácticas con ReactJs y NextJs. Realicé un bootcamp en la empresa <a href='https://cesticom.com/'>Cesticom</a>, en donde aprendí y demostré mis habilidades para la contrucción de API REST con Node, Express y MongoDB.
+                        Soy desarrollador Web, especializado en los Stacks MERN y MEVN. Inicié mis estudios en <a href='https://platzi.com/p/Griger/'>Platzi</a>, donde desarrollé mis capacidades para crear interfaces de usuario usando buenas prácticas con ReactJs y NextJs. Realicé un bootcamp en la empresa <a href='https://cesticom.com/'>Cesticom</a>, en donde aprendí y demostré mis habilidades para la construcción de API REST con Node, Express y MongoDB.
                     </p>
                 </Details>
         </section>

7c8f066d1b5a9567e7cfedbc1eccdfa72827aaa4
Griger Ratia <grigerratia@gmail.com>
2023-06-21 14:25:31 -0400

    Reparando bug de Logo y el texo al hacer chik multiples veces

--- src/components/components/Logo.jsx
+++ src/components/components/Logo.jsx
@@ -1,40 +1,37 @@
-import { useState } from 'react'
-import '../../styles/styles-components/Logo.css'
-import THANKS from '../../utils/thanks'
+import { useState } from "react";
+import "../../styles/styles-components/Logo.css";
+import THANKS from "../../utils/thanks";
 
-function Logo(){
+function Logo() {
+	const [className, setClassName] = useState("msg-desactive");
+	const [message, setMessage] = useState(THANKS[0]);
+	const [aNew, setANew] = useState("new-news");
 
-    const [className, setClassName] = useState('msg-desactive')
-    const [message, setMessage] = useState(THANKS[0])
-    const [aNew, setANew] = useState('new-news')
+	const vewMessage = () => {
+		className === "msg-desactive"
+			? (setClassName("msg-active"), setANew("not-news"))
+			: (setClassName("msg-desactive"), setNotice());
+	};
 
-    const vewMessage = () => {
-        className === 'msg-desactive'
-            ? (
-                setClassName('msg-active'),
-                setANew('not-news')
-            ) : (
-                setClassName('msg-desactive'),
-                setNotice()
-            )
-    }
+	const setNotice = () => {
+		setTimeout(() => {
+			setMessage(THANKS[Math.floor(Math.random() * (9 - 0))]);
+			setANew("new-news");
+		}, 10000);
+	};
 
-    const setNotice = () => {
-        setTimeout(()=> {
-            setMessage(THANKS[Math.floor(Math.random() * (9 - 0))])
-            setANew('new-news')
-        }, 10000)
-    }
-
-    return (
-        <div className='logo' onClick={ vewMessage } >
-            <img src="https://thumbs2.imgbox.com/16/75/BrVkQVSF_t.jpg" alt="Logo Griger"/>
-            <div className={ aNew }>1</div>
-            <a href='#' className={ className } >
-                { message }
-            </a>
-        </div>
-    )
+	return (
+		<div className='logo' onClick={vewMessage}>
+			<img
+				src='https://thumbs2.imgbox.com/16/75/BrVkQVSF_t.jpg'
+				alt='Logo Griger'
+			/>
+			<div className={aNew}>1</div>
+			<a href='#' className={className}>
+				{message}
+			</a>
+		</div>
+	);
 }
 
-export default Logo
+export default Logo;

9b8c5eb482f6686302465f802ac6753310651dcb
Griger Ratia <grigerratia@gmail.com>
2023-07-08 00:44:42 -0400

    Terminada la descripcion de los proyectos

--- src/utils/myproyects.jsx
+++ src/utils/myproyects.jsx
@@ -1,30 +1,32 @@
+import Anchor from "../components/components/Anchor";
+
 const PROYECTS = [
 	{
 		name: "Twitter Clone",
 		colorPointer: "#032731",
 		imgProyect:
 			"https://d1ih8jugeo2m5m.cloudfront.net/2022/04/tipos-de-paginas-web-1200x685.jpg",
-		years: "2023 . 2021",
+		years: "2020 . 2021",
 		texto:
-			"Este proyecto está hecho en React. Fue un reto el manejo del estado y la creación de componentes, pero fue un desafío superado exitoamente.",
+			"Este proyecto está hecho en React. Fue un reto el manejo del estado y la creación de componentes, pero ya es un desafío superado exitoamente.",
 	},
 	{
 		name: "Tic Tac Toe",
 		colorPointer: "#032731",
 		imgProyect:
 			"https://d1ih8jugeo2m5m.cloudfront.net/2022/04/tipos-de-paginas-web-1200x685.jpg",
-		years: "2023 . 2021",
+		years: "2021 . 2022",
 		texto:
 			"En mis inicios usando React, como todo principiante, no faltó esta aplicación. Aquí se definen todos los conceptos de esta popular librería.",
 	},
 	{
-		name: "Store",
+		name: "SaverSaver",
 		colorPointer: "#032731",
 		imgProyect:
 			"https://d1ih8jugeo2m5m.cloudfront.net/2022/04/tipos-de-paginas-web-1200x685.jpg",
-		years: "2023 . 2021",
+		years: "2022 . 2023",
 		texto:
-			"lorem ipsum dolor sit amet, consect lorem ipsum dolor sit amet, consect lorem ipsum dolor sit amet, consect lorem ipsum dolor sit amet, consect",
+			"SaverSaver es uno de mis proyectos estrella. Uno con el que sueño independizarme por completo. Al verlo, notarás que le he puesto corazón.",
 	},
 	{
 		name: "Todo List",
@@ -33,25 +35,25 @@ const PROYECTS = [
 			"https://d1ih8jugeo2m5m.cloudfront.net/2022/04/tipos-de-paginas-web-1200x685.jpg",
 		years: "2023 . 2021",
 		texto:
-			"lorem ipsum dolor sit amet, consect lorem ipsum dolor sit amet, consect lorem ipsum dolor sit amet, consect lorem ipsum dolor sit amet, consect",
+			"Todos han hecho un ToDo List, el asunto radica en qué tiene de especial el código y las funcionalidades de cada quién. Averigua los detalles interesantes del mío.",
 	},
 	{
-		name: "Traslate Money",
+		name: "Forecaster",
 		colorPointer: "#032731",
 		imgProyect:
 			"https://d1ih8jugeo2m5m.cloudfront.net/2022/04/tipos-de-paginas-web-1200x685.jpg",
 		years: "2023 . 2021",
 		texto:
-			"lorem ipsum dolor sit amet, consect lorem ipsum dolor sit amet, consect lorem ipsum dolor sit amet, consect lorem ipsum dolor sit amet, consect",
+			"Este proyecto es un buen ejemplo para mostrar mis habilidades para consumir APIs con eficiencia. También notarás la agradable presentaición de front.",
 	},
 	{
-		name: "Betel Church",
+		name: "Healthy",
 		colorPointer: "#032731",
 		imgProyect:
 			"https://d1ih8jugeo2m5m.cloudfront.net/2022/04/tipos-de-paginas-web-1200x685.jpg",
 		years: "2023 . 2021",
 		texto:
-			"lorem ipsum dolor sit amet, consect lorem ipsum dolor sit amet, consect lorem ipsum dolor sit amet, consect lorem ipsum dolor sit amet, consect",
+			"Este proyecto, es una idea de red social dirigida a toda persona que quiera dejar de consumir contenido inapropiado, innecesario o controvertido. Creo que es ideal para jóvenes y niños.",
 	},
 	{
 		name: "Condostart",
@@ -59,8 +61,20 @@ const PROYECTS = [
 		imgProyect:
 			"https://d1ih8jugeo2m5m.cloudfront.net/2022/04/tipos-de-paginas-web-1200x685.jpg",
 		years: "2023 . 2021",
+		texto: [
+			`Condostart es un proyecto en el que trabajé miestras desarrollaba para `,
+			<Anchor key='Anchor-1' text='Cesticom' anchor='https://cesticom.com/' />,
+			`. Es uno de esos retos en mi vida que recuerdo de forma especial, por el trabajo en equipo y la superación personal. `,
+		],
+	},
+	{
+		name: "Iglesia Betel",
+		colorPointer: "#032731",
+		imgProyect:
+			"https://d1ih8jugeo2m5m.cloudfront.net/2022/04/tipos-de-paginas-web-1200x685.jpg",
+		years: "2023 . 2021",
 		texto:
-			"lorem ipsum dolor sit amet, consect lorem ipsum dolor sit amet, consect lorem ipsum dolor sit amet, consect lorem ipsum dolor sit amet, consect",
+			"Soy cristiano y fiel seguidor de las enseñanzas de Dios y Jesucristo. Me congrego en la Iglesia Bíblica Bautista Betel, así que puedes hacerte una idea de este proyecto. Es una página para darla a conocer. Eso y otras cosas.",
 	},
 ];
 
